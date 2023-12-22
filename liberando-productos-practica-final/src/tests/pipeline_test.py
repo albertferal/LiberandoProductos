b1cbe07468a5e9bd0f9c65a796e92848dc838eeb
albertferal <albertferal@gmail.com>
2023-12-22 20:28:47 +0100

    testing

---
+++ liberando-productos-practica-final/src/tests/pipeline_test.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+import pytest
+
+from application.app import app
+
+client = TestClient(app)
+
+@pytest.mark.asyncio
+async def test_read_health():
+    """Tests the health check endpoint"""
+    response = client.get("health")
+
+    assert response.status_code == 200
+    assert response.json() == {"health": "ok"}
+
+@pytest.mark.asyncio
+async def test_read_main():
+    """Tests the main endpoint"""
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+@pytest.mark.asyncio
+async def test_read_bye():
+    """Tests the bye endpoint"""
+    response = client.get("/bye")
+
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Bye, have a nice day"}
